8bd87062b14ac5df1e2249ee6aa628384a1e0af3
Rui Castro <rui_castro@live.com.pt>
2023-08-04 17:35:11 +0000

    Working datepicker

---
+++ script/components/datepicker/datepicker.js
@@ -0,0 +1,34 @@
+import { showDate } from "../../events/dateEmitter.js";
+
+export function datepicker() {
+  const TODAY = new Date();
+  const NEXT_YEAR = new Date(
+    TODAY.getFullYear() + 1,
+    TODAY.getMonth(),
+    TODAY.getDate()
+  )
+    .toISOString()
+    .slice(0, 10);
+
+  let day;
+  let month;
+  let year;
+
+  const datepicker = document.createElement("input");
+  datepicker.setAttribute("type", "date");
+  datepicker.setAttribute("id", "datepicker");
+  datepicker.setAttribute("value", new Date().toISOString().slice(0, 10));
+  datepicker.setAttribute("min", new Date().toISOString().slice(0, 10));
+  datepicker.setAttribute("max", NEXT_YEAR);
+
+  datepicker.addEventListener("change", () => {
+    day = datepicker.valueAsDate.getDate();
+    month = datepicker.valueAsDate.getMonth();
+    year = datepicker.valueAsDate.getFullYear();
+    const date = new Date(year, month, day).toISOString().slice(0, 10);
+
+    showDate(date);
+  });
+
+  return datepicker;
+}
