f2dad20e62dad4e5f34b8fef00a3e9537b0c424b
Rui Castro <rui_castro@live.com.pt>
2023-08-04 18:14:42 +0000

    Added comments to the Event Emitter

--- script/EventEmitter.js
+++ script/EventEmitter.js
@@ -2,25 +2,25 @@
 
 /**
  * @class EventEmitter
- * @description Event emitter class
- * @example
- * const emitter = new EventEmitter();
- * emitter.on('event', () => console.log('emitting!'));
- * emitter.emit('event'); // 'emitting!'
- *
+ * @description State management class that allows for event-driven programming.
  */
 
 export class EventEmitter {
   constructor() {
     this.events = {};
   }
-  on(event, listener) {
-    if (typeof this.events[event] !== "object") {
-      this.events[event] = [];
-    }
-    this.events[event].push(listener);
-    return () => this.removeListener(event, listener);
-  }
+
+  /**
+   * @method on
+   * @description Adds a listener to the event queue for a given event.
+   * @param {string} event - The event name.
+   * @param {function} listener - The callback function.
+   * @returns {removeListener} A function that removes the listener from the event queue.
+   * @example
+   * emitter.on('event', () => console.log('Hello there!'));
+   * emitter.emit('event'); // 'Hello there!'
+   */
+
   removeListener(event, listener) {
     if (typeof this.events[event] === "object") {
       const idx = this.events[event].indexOf(listener);
@@ -29,11 +29,40 @@ export class EventEmitter {
       }
     }
   }
+
+  on(event, listener) {
+    if (typeof this.events[event] !== "object") {
+      this.events[event] = [];
+    }
+    this.events[event].push(listener);
+    return () => this.removeListener(event, listener);
+  }
+
+  /**
+   * @method emit
+   * @description Emits an event and calls each of the listeners registered for it.
+   * @param {string} event - The event name.
+   * @param {...*} args - Arguments to be passed to the callback functions.
+   * * @example
+   * emitter.once('event', () => console.log('One emit to rule them all!'));
+   * emitter.emit('event'); // 'One emit to rule them all!'
+   * emitter.emit('event'); // Doesn't emit anything.
+   */
+
   emit(event, ...args) {
     if (typeof this.events[event] === "object") {
       this.events[event].forEach((listener) => listener.apply(this, args));
     }
   }
+
+  /**
+   * @method once
+   * @description Adds a one-time listener to the event queue for a given event.
+   * @param {string} event - The event name.
+   * @param {function} listener - The callback function.
+   * @returns {removeListener} A function that removes the listener from the event queue.
+   **/
+
   once(event, listener) {
     const remove = this.on(event, (...args) => {
       remove();
