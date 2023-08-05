8c39dc546240d5cd5a461fbddab39a5116b025af
Rui Castro <rui_castro@live.com.pt>
2023-08-05 20:01:26 +0000

    Add form validation to search input and datepicker

--- script/components/datepicker/datepicker.js
+++ script/components/datepicker/datepicker.js
@@ -16,24 +16,32 @@ export function datepicker() {
     TODAY.getFullYear() + 1,
     TODAY.getMonth(),
     TODAY.getDate()
-  )
-    .toISOString()
-    .slice(0, 10);
+  );
 
   const datepicker = document.createElement("input");
   datepicker.setAttribute("type", "date");
   datepicker.setAttribute("id", "datepicker");
   datepicker.setAttribute("value", TODAY.toISOString().slice(0, 10));
   datepicker.setAttribute("min", TODAY.toISOString().slice(0, 10));
-  datepicker.setAttribute("max", NEXT_YEAR);
+  datepicker.setAttribute("max", NEXT_YEAR.toISOString().slice(0, 10));
 
   datepicker.addEventListener("change", () => {
-    day = datepicker.valueAsDate.getDate();
-    month = datepicker.valueAsDate.getMonth();
-    year = datepicker.valueAsDate.getFullYear();
+    const selectedDate = datepicker.valueAsDate;
 
-    const newDate = new Date(year, month, day).toISOString().slice(0, 10);
-    showDate(newDate);
+    if (selectedDate < TODAY) {
+      datepicker.preventDefault;
+      alert("Please select a date that is today or later.");
+    } else if (selectedDate > NEXT_YEAR) {
+      datepicker.preventDefault;
+      alert("Please select a date within the next year.");
+    } else {
+      day = datepicker.valueAsDate.getDate();
+      month = datepicker.valueAsDate.getMonth();
+      year = datepicker.valueAsDate.getFullYear();
+
+      const newDate = new Date(year, month, day).toISOString().slice(0, 10);
+      showDate(newDate);
+    }
   });
 
   return datepicker;
