4b677eb1832ab35bba4d4304abace75a2f4a65c4
Rui Castro <rui_castro@live.com.pt>
2023-08-06 16:16:36 +0000

    Changed layout.js to main.js

--- script/components/searchInputBox/SearchInput.js
+++ script/components/searchInputBox/SearchInput.js
@@ -39,12 +39,25 @@ export function SearchInput() {
     })
   );
 
-  inputBox.addEventListener("focusout", () => {
-    if (inputBox.value === "") {
+  inputBox.addEventListener("change", () => {
+    let showWarning = false;
+
+    if (!search) {
+      showWarning = true;
+    }
+
+    if (showWarning === true) {
       const warning = document.createElement("span");
-      warning.classList.add("warning");
+      warning.setAttribute("id", "warning");
       warning.textContent = "Please choose a destination";
-      inputBox.parentNode.appendChild(warning);
+      inputBox.after(warning);
+    }
+
+    if (showWarning === false) {
+      const warning = document.getElementById("warning");
+      if (warning) {
+        warning.remove();
+      }
     }
   });
 
